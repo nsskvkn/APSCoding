7adffd29c5c433edc4467a34d4ab7666a44464c7
nsskvkn <d9127506@gmail.com>
2026-05-02 21:49:01 +0300

    implement Wilderness habitat without caregiving

--- Lab1/Models/Wilderness.cs
+++ Lab1/Models/Wilderness.cs
@@ -3,5 +3,14 @@ namespace Lab1
     public class Wilderness : Habitat
     {
         public Wilderness() : base("Wild") { }
+
+        public override void AddAnimal(Animal a)
+        {
+            base.AddAnimal(a);
+            a.SetWild(); // Тварина на волі автоматично щаслива
+        }
+        
+        // Немає FeedAll/CleanAll
+
     }
 }
