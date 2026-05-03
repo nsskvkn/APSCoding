941a607f28e6fe17aeed8860dcc60dbe7e86d05a
nsskvkn <d9127506@gmail.com>
2026-05-03 19:18:36 +0300

    add AnimalFactory extensible with registration

--- Lab1/AnimalFactory.cs
+++ Lab1/AnimalFactory.cs
@@ -4,17 +4,29 @@ using Lab1;
 namespace Lab1
 {
     public static class AnimalFactory
+{
+    private static Dictionary<string, Func<string, Animal>> registry = new()
     {
-        private static Dictionary<string, Func<string, Animal>> map = new()
-        {
-            { "dog", name => new Dog(name) },
-            { "owl", name => new Owl(name) },
-            { "lizard", name => new Lizard(name) }
-        };
+        { "dog", name => new Dog(name) },
+        { "owl", name => new Owl(name) },
+        { "lizard", name => new Lizard(name) }
+    };
 
-        public static Animal Create(string type, string name)
-        {
-            return map[type.ToLower()](name);
-        }
+    // Можливість додати новий тип без зміни існуючого коду
+    public static void Register(string type, Func<string, Animal> creator)
+    {
+        registry[type.ToLower()] = creator;
     }
+
+    public static Animal Create(string type, string name)
+    {
+        if (!registry.ContainsKey(type.ToLower()))
+            throw new ArgumentException($"Unknown animal type: {type}");
+        
+        return registry[type.ToLower()](name);
+    }
+
+    public static IEnumerable<string> GetAvailableTypes() => registry.Keys;
+}
+
 }
